d0ad8fa4ef958782e02a9d74ee13cc42ab5dfd75
lichun <li-chun-1996@live.cn>
2015-01-06 20:29:38 +0800

    deployment openshift

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bootcamp',
+      version='1.0',
+      description='OpenShift App',
+      author='lichun',
+      author_email='li-chun-1996@live.cn',
+      url='http://bootcamp.lichun.me/',
+)
