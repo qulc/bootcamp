c16f9af850b48f8b4cd8caf49fcd8a1c3a6c6ce9
lichun <i@lichun.me>
2015-03-12 15:58:15 +0800

    update

--- bootcamp/messages/views.py
+++ bootcamp/messages/views.py
@@ -1,7 +1,6 @@
 import json
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
