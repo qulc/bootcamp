05f5d7d982b172b76efbcc05ae8c0b32c4888ac4
lichun <li-chun-1996@live.cn>
2015-04-27 10:47:30 +0800

    update bootcamp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 # Put here required packages or
 # Uncomment one or more lines below in the install_requires section
 # for the specific client drivers/modules your application needs.
-packages = ['Django<=1.6',
+packages = ['Django<=1.7.7',
             'static3',  # If you want serve the static files in the same server
             # 'mysql-connector-python',
             # 'pymongo',
@@ -18,7 +18,7 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
 
 setup(name='bootcamp',
       version='1.0',
-      description='OpenShift Python2.7 App. Django-1.6 Community Cartridge based application',
+      description='OpenShift Python2.7 App. Django-1.7 Community Cartridge based application',
       author='lichun',
       author_email='li-chun-1996@live.cn',
       url='http://bootcamp.lichun.me/',
