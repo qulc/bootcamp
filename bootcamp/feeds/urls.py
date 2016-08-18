24f610a77e39f6d0a964bfb873e42f80301fee95
lichun <i@lichun.me>
2016-08-18 22:46:35 +0800

    add core urls.py

--- bootcamp/feeds/urls.py
+++ bootcamp/feeds/urls.py
@@ -1,20 +1,17 @@
 from django.conf.urls import url
 
-from .views import (
-    feeds, post, like, comment, load, check,
-    load_new, update, track_comments, remove, feed
-)
+from .import views
 
 urlpatterns = [
-    url(r'^$', feeds, name='feeds'),
-    url(r'^(\d+)/$', feed, name='feed'),
-    url(r'^post/$', post, name='post'),
-    url(r'^like/$', like, name='like'),
-    url(r'^load/$', load, name='load'),
-    url(r'^check/$', check, name='check'),
-    url(r'^update/$', update, name='update'),
-    url(r'^comment/$', comment, name='comment'),
-    url(r'^remove/$', remove, name='remove_feed'),
-    url(r'^load_new/$', load_new, name='load_new'),
-    url(r'^track_comments/$', track_comments, name='track_comments'),
+    url(r'^$', views.feeds, name='feeds'),
+    url(r'^(\d+)/$', views.feed, name='feed'),
+    url(r'^post/$', views.post, name='post'),
+    url(r'^like/$', views.like, name='like'),
+    url(r'^load/$', views.load, name='load'),
+    url(r'^check/$', views.check, name='check'),
+    url(r'^update/$', views.update, name='update'),
+    url(r'^comment/$', views.comment, name='comment'),
+    url(r'^remove/$', views.remove, name='remove_feed'),
+    url(r'^load_new/$', views.load_new, name='load_new'),
+    url(r'^track_comments/$', views.track_comments, name='track_comments'),
 ]
--- bootcamp/questions/urls.py
+++ bootcamp/questions/urls.py
@@ -1,19 +1,16 @@
 from django.conf.urls import url
 
-from .views import (
-    question, answer, answered, unanswered, ask,
-    all_question, questions, favorite, accept, vote
-)
+from . import views
 
 urlpatterns = [
-    url(r'^$', questions, name='questions'),
-    url(r'^answered/$', answered, name='answered'),
-    url(r'^unanswered/$', unanswered, name='unanswered'),
-    url(r'^all/$', all_question, name='all'),
-    url(r'^ask/$', ask, name='ask'),
-    url(r'^favorite/$', favorite, name='favorite'),
-    url(r'^answer/$', answer, name='answer'),
-    url(r'^answer/accept/$', accept, name='accept'),
-    url(r'^answer/vote/$', vote, name='vote'),
-    url(r'^(\d+)/$', question, name='question'),
+    url(r'^$', views.questions, name='questions'),
+    url(r'^answered/$', views.answered, name='answered'),
+    url(r'^unanswered/$', views.unanswered, name='unanswered'),
+    url(r'^all/$', views.all_question, name='all'),
+    url(r'^ask/$', views.ask, name='ask'),
+    url(r'^favorite/$', views.favorite, name='favorite'),
+    url(r'^answer/$', views.answer, name='answer'),
+    url(r'^answer/accept/$', views.accept, name='accept'),
+    url(r'^answer/vote/$', views.vote, name='vote'),
+    url(r'^(\d+)/$', views.question, name='question'),
 ]
